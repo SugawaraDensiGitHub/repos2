66e8b22b9c985463219c8c013a451eec994a4806
sugarara <sugarara@jec.ac.jp>
2020-10-20 11:41:12 +0900

    Hello SQLを追加

--- src/gitEx01/HelloJava.java
+++ src/gitEx01/HelloJava.java
@@ -5,7 +5,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello Java");
 		System.out.println("Hello PHP");
-
+		System.out.println("Hello SQL");
 	}
 
 }
